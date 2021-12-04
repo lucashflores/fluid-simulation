9d76949af92c515b0dd2109ca29fe6e1a5568c71
Lucas <lucas.henrique.flores2001@gmail.com>
2021-12-04 18:35:35 -0300

    addSource and setBounds implemented

--- include/Fluid.h
+++ include/Fluid.h
@@ -1,5 +1,27 @@
 #pragma once
 
+#include "stdafx.h"
+#include <vector>
+
 class Fluid {
+private:
+    int height;
+    int width;
+    int size;
+    float diff;
+    std::vector<float> source;
+    std::vector<float> initialDensity;
+    std::vector<float> density;
+    std::vector<float> hvelocity;
+    std::vector<float> vvelocity;
+public:
+    Fluid(int w, int h, float diff);
+    ~Fluid();
+    void setSize(int w, int h);
+    void addSource(float dt);
+    void diffuse(float dt);
+    void project();
+    void abvect();
+    void setBounds(int direction);
 
 };
--- src/Fluid.cpp
+++ src/Fluid.cpp
@@ -1,2 +1,54 @@
+#include "Fluid.h"
 
+Fluid::Fluid(int w, int h, float diff): height(h), width(w), diff(diff), density(), initialDensity(), source(), hvelocity(), vvelocity() {
+    setSize(w, h);
+    density.assign(size, 0);
+    source.assign(size, 0);
+    hvelocity.assign(size, 0);
+    vvelocity.assign(size, 0);
+}
+
+Fluid::~Fluid() {
+    density.clear();
+    initialDensity.clear();
+    source.clear();
+    hvelocity.clear();
+    vvelocity.clear();
+}
+
+
+
+void Fluid::setSize(int w, int h) {
+    size = (w+2)*(h+2);
+}
+
+void Fluid::addSource(float dt) {
+    for (int i = 0; i < size; i++) {
+        density[i] += dt*source[i];
+    }
+}
+
+void Fluid::setBounds(int direction) {
+    /*
+    1 - horizontal
+    2 - vertical
+    */
+    for (int i = 1; i <= width; i++) {
+        density[IX(0, i, height)] = direction == 1? -density[IX(1, i, height)]:density[IX(1, i, height)];
+        density[IX(width + 1, i, height)] = direction == 1? -density[IX(width, i, height)]:density[IX(width, i, height)];
+    }
+    for (int i = 1; i <= height; i++) {
+        density[IX(i, 0, height)] = direction == 2? -density[IX(i, 1, height)]:density[IX(i, 1, height)];
+        density[IX(i, height + 1, height)] = direction == 2? -density[IX(i, height, height)]:density[IX(i, height, height)];
+    }
+    //Canto superior esquerdo
+    density[IX(0, 0, height)] = (density[IX(0, 1, height)] + density[IX(1, 0, height)])/2;
+    //Canto superior direito
+    density[IX(width + 1, 0, height)] = (density[IX(width + 1, 1, height)] + density[IX(width, 0, height)])/2;
+    //Canto inferior esquerdo
+    density[IX(0, height + 1, height)] = (density[IX(0, height, height)] + density[IX(1, height + 1, height)])/2;
+    //Canto inferior direito
+    density[IX(width + 1, height + 1, height)] = (density[IX(width, height + 1, height)] + density[IX(width + 1, height, height)])/2;
+
+}
 
