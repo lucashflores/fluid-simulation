23d767d5fed92c9086678c0f8c7f501073747863
Lucas <lucas.henrique.flores2001@gmail.com>
2021-12-04 16:43:02 -0300

    initial files

---
+++ cmake-build-debug/CMakeFiles/fluid-simulation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fluid-simulation.dir/main.cpp.obj"
+  "fluid-simulation.exe"
+  "fluid-simulation.exe.manifest"
+  "fluid-simulation.pdb"
+  "libfluid-simulation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fluid-simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
